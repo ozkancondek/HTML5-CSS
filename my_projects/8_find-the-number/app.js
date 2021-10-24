8eaa5316d8ab37032d851609c86e52e918aa7438
ozkancondek <ozkncndek@gmail.com>
2021-10-24 12:08:50 +0200

    guess a number

--- my_projects/8_find-the-number/app.js
+++ my_projects/8_find-the-number/app.js
@@ -1,14 +1,32 @@
 const guess = function(num){
 
-    let real = 7;
+    let real = 17;
     if(real < num ){
        return  "Your guess is bigger!"
     }else if(real > num ){
        return  "Your guess is smaller!"
 
     }else{
-        return "You win"
+        return "Congratulations!!!"
     } 
 
                                    
+}
+
+const button = document.getElementById("btn");
+
+const input = document.getElementById("input");
+
+const par = document.getElementById("par");
+
+const par1 = document.getElementById("par");
+
+
+button.onclick = function(){
+     
+    
+    par.innerText = guess(input.value)
+     
+
+
 }
