0da71907429f4d709bd1816b65ca1077344a271e
ozkancondek <ozkncndek@gmail.com>
2021-10-24 09:56:43 +0200

    find the number project html side

---
+++ my_projects/8_find-the-number/app.js
@@ -0,0 +1,14 @@
+const guess = function(num){
+
+    let real = 7;
+    if(real < num ){
+       return  "Your guess is bigger!"
+    }else if(real > num ){
+       return  "Your guess is smaller!"
+
+    }else{
+        return "You win"
+    } 
+
+                                   
+}
