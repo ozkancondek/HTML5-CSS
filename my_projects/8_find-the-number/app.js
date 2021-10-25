32f2f451a6e1a38a61e7d196cdfe0957f79b4f5f
ozkancondek <ozkncndek@gmail.com>
2021-10-25 23:50:05 +0200

    guess a number game

--- my_projects/8_find-the-number/app.js
+++ my_projects/8_find-the-number/app.js
@@ -1,31 +1,41 @@
-const guess = function(num){
+ 
 
-    let real = 17;
-    if(real < num ){
-       return  "Your guess is bigger!"
-    }else if(real > num ){
-       return  "Your guess is smaller!"
-
-    }else{
-        return "Congratulations!!!"
-    } 
-
-                                   
-}
-
-const button = document.getElementById("btn");
+const button = document.getElementById("button");
 
 const input = document.getElementById("input");
 
-const par = document.getElementById("par");
+const third = document.getElementById("third");
 
-const par1 = document.getElementById("par");
+const fourth = document.getElementById("fourth");
 
 
+
+ 
 button.onclick = function(){
+     let value = input.value;
+     let real = 17;
+     while(c<5){
+        if(real < num ){
+            fourth.innerHTML = "Your guess is bigger!"
+            third.innerHTML += value
+         }else if(real > num ){
+            fourth.innerHTML = "Your guess is smaller!"
+            third.innerHTML += value
      
+         }else{
+            fourth.innerHTML = "⭐Congratulations⭐"
+            fourth.innerHTML = `You made ${c} attempts totaly.`
+
+            break
+         } 
+
+
+
+
+         c++;
+     }
     
-    par.innerText = guess(input.value)
+    fourth.innerText = guess(input.value)
      
 
 
