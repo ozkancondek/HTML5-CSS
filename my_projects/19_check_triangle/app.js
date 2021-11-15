0563ba51454a138a42ef87cf8d247ccbe1387423
ozkancondek <ozkncndek@gmail.com>
2021-11-15 22:09:01 +0100

    triangle check

--- my_projects/19_check_triangle/app.js
+++ my_projects/19_check_triangle/app.js
@@ -1,70 +1,50 @@
-
-
-
-
+const side1 = document.querySelectorAll(".form-control")[0];
+const side2 = document.querySelectorAll(".form-control")[1];
+const side3 = document.querySelectorAll(".form-control")[2];
+const div = document.getElementById("addhere");
 
 const button = document.getElementById("btn");
 
-const addHere = document.getElementById("addhere");
+button.addEventListener("click", func1);
 
+function func1() {
+  let [a, b, c] = [side1.value, side2.value, side3.value];
+  let vars = [+a, +b, +c];
 
-class Triangle {
-    constructor() {
-        this.side1 = document.querySelectorAll(".form-control")[0].value
-        this.side2 = document.querySelectorAll(".form-control")[1].innerText;
-        this.side3 = document.querySelectorAll(".form-control")[2].value;
-        this.div = document.getElementById("addhere");
-
-
+  if (side1.value == "" || side1.value == "" || side1.value == "") {
+    div.innerHTML += `<div class="alert alert-danger" role="alert">
+          Please enter all sides!
+        </div>`;
+  } else if (
+    (vars[0] + vars[1] > vars[2]) &
+    (vars[2] + vars[2] > vars[0]) &
+    (vars[2] + vars[0] > vars[1])
+  ) {
+    if (vars.filter((x) => x == vars[0]).length == 3) {
+      div.innerHTML += `<div class="alert alert-primary" role="alert">
+                  This triangle is equilateral.
+                    </div>`;
+    } else if (
+      vars.filter((x) => x == vars[0]).length == 2 ||
+      vars.filter((x) => x == vars[1]).length == 2 ||
+      vars.filter((x) => x == vars[2]).length == 2
+    ) {
+      div.innerHTML += `<div class="alert alert-primary" role="alert">
+                    This triangle is isoscele.
+                </div>`;
+    } else if (
+      (vars.filter((x) => x == vars[0]).length == 1) &
+      (vars.filter((x) => x == vars[1]).length == 1) &
+      (vars.filter((x) => x == vars[2]).length == 1)
+    ) {
+      div.innerHTML += `<div class="alert alert-primary" role="alert">
+                     This triangle is scalene.
+                      </div>`;
     }
-    check() {
-        return this.side1
-        // if ((this.side1 + this.side2 > this.side3)
-        //     & (this.side1 + this.side3 > this.side2)
-        //     & (this.side2 + this.side3 > this.side1)) {
-
-        //     if (this.side1 == this.side2 & this.side1 == this.side3 & this.side2 == this.side3) {
-        //         addHere.innerHTML += `<div class="alert alert-primary" role="alert">
-        //              This triangle is equilateral.
-        //             </div>`
-        //     }
-        //     else if ((this.side1 == this.side2 & this.side1 != this.side3)
-        //         || (this.side1 == this.side3 & this.side1 != this.side2)
-        //         || (this.side2 == this.side1 & this.side2 != this.side3)
-        //         || (this.side2 == this.side3 & this.side2 != this.side1)
-        //         || (this.side3 == this.side1 & this.side3 != this.side2)
-        //         || (this.side3 == this.side2 & this.side3 != this.side1)) {
-        //         addHere.innerHTML += `<div class="alert alert-primary" role="alert">
-        //              This triangle is isoscele.
-        //             </div>`
-        //     }
-        //     else {
-        //         addHere.innerHTML += `<div class="alert alert-primary" role="alert">
-        //              This triangle is scalene.
-        //             </div>`
-
-        //     }
-
-        // }
-        //         else {
-        //             addHere.innerHTML += `<div class="alert alert-danger" role="alert">
-        //   This is not a triangle!
-        // </div>`
-        //         }
-
-
-    }
-
-
+  } else {
+    div.innerHTML += `<div class="alert alert-danger" role="alert">
+          This is not a triangle!
+        </div>`;
+  }
+  console.log(5);
 }
-const result = new Triangle();
-
-
-
-
-
-
-button.addEventListener("click", function () {
-    console.log(result.side1);
-});
-
